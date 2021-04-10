766ce5caf6f11a1d02101fd1839ab68070d63efe
John Jaramillo <jaramillo784@gmail.com>
2021-04-09 22:34:08 -0400

    created employee Directory where all other components will be rendered

--- components/EmployeeDirectory.js
+++ components/EmployeeDirectory.js
@@ -1,12 +1,47 @@
-import React, {Component, component} from 'react';
+import React, { Component, component } from 'react';
 
 
 class EmployeeDirectory extends Component {
-state = {
-    search= "",
-    results= []
-}
+    state = {
+        search= "",
+        results=[]
+    };
 
+    componentDidMount() {
+        this.searchRandomUser('Adam');
+    };
+
+    searchRandomUser = query => {
+        API.search(query)
+            .then(res => this.setState({ results: res.data.data }))
+            .catch(err => console.log(err));
+    };
+
+    handleInputChange = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]: value
+        })
+    };
+
+    // When the form is submitted search the Random User API for "this.state.search"
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.searchRandomUser(this.state.search);
+    };
+
+    render() {
+        return (
+            <>
+            <hero>
+                
+            </hero>
+            {/* Search Component */}
+            {/* Table with results Component */}
+            </>
+        )
+    }
 }
 
 export default EmployeeDirectory;
